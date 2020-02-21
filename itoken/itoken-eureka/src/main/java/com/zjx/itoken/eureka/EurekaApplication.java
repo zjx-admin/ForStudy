99a4b3ed5807010f513ca5f866135bbb58b73869
zjx <986379932@qq.com>
2020-02-22 00:05:31 +0800

    第95集笔记

---
+++ itoken/itoken-eureka/src/main/java/com/zjx/itoken/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.zjx.itoken.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer            //开启服务端
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ itoken/itoken-zuul/src/main/java/com/zjx/itoken/zuul/provider/FeignFallbackProvider.java
@@ -0,0 +1,71 @@
+package com.zjx.itoken.zuul.provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class FeignFallbackProvider implements FallbackProvider {
+    @Override
+    public String getRoute() {                                     //回调方法，路由失败时调用，返回需要被处理的服务
+        return "hello-spring-cloud-service-consumer-feign";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse(){
+            /**
+             * 网关向 api 服务请求失败了，但是消费者客户端向网关发起的请求是成功的，
+             * 不应该把 api 的 404,500 等问题抛给客户端
+             * 网关和 api 服务集群对于客户端来说是黑盒
+             * @return
+             * @throws IOException
+             */
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {           //错误相应内容
+                ObjectMapper objectMapper = new ObjectMapper();          //用于以字符串形式将map输出
+                Map<String, Object> map = new HashMap<>();
+                map.put("status", 200);
+                map.put("message", "无法连接，请检查您的网络");
+                return new ByteArrayInputStream(objectMapper.writeValueAsString(map).getBytes("UTF-8"));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {                  //错误相应请求头
+                HttpHeaders headers = new HttpHeaders();
+                // 和 getBody 中的内容编码一致
+                headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+                return headers;
+            }
+        };
+    }
+}
